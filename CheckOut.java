cb703739153818b091a27c20001e35790b3db2f4
Caroline Zheng <zheng37c@gmail.com>
2025-11-15 17:53:43 -0800

    Update CheckOut.java

--- CheckOut.java
+++ CheckOut.java
@@ -1,6 +1,6 @@
-import java.util.*;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.*;
 
 public class CheckOut{
     private LibraryInventory inventory;
@@ -41,7 +41,13 @@ public class CheckOut{
     
     public void storeInfo(String name){
         int[] location = getLocation(name);
-        try(){}
+        try(FileWriter writer = new FileWriter("checkouts.txt", true)){
+            writer.write("Patron: " + patron + "\n");
+            writer.write("Book: " + name + "\n");
+            writer.write("Due Date: " + dueDate + "\n");
+            writer.write("Location: [" + location[0] + ", " + location[1] + "]\n");
+            writer.write("----------------------\n");
+        }
         catch(IOException e){
             System.out.println("Invalid information, unable to store data");
         }
